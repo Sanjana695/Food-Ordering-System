827c0d28d90a32890431d07acbf13270a00bc127
Sanjana695 <sanjana.bcsf18@iba-suk.edu.pk>
2022-09-15 22:57:39 +0500

    Food ordering web-application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,37 +5,36 @@ require("./db/conn");
 // const User = require("")
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //it will recognize incoming request as a json object
-app.use(express.json())
+app.use(express.json());
 
-//post menus api 
- app.post("/menus",async(req,res)=>{
-    const item=await Menus(req.body);
-    //save new created menu in database
-    item.save().then(()=>{
+//post menus api
+app.post("/menus", async (req, res) => {
+  const item = await Menus(req.body);
+  //save new created menu in database
+  item
+    .save()
+    .then(() => {
       //201-->the response has succeded and a new resource has been created
-     res.status(201).send(item)
-     
-   }).catch((e)=>{
-     res.status(400).send(e);
-     console.log(item);
-   })
-   
- })
-
+      res.status(201).send(item);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+      console.log(item);
+    });
+});
 
 //get menus api
 app.get("/menus", async (req, res) => {
   try {
     const menuItem = await Menus.find();
     res.send(menuItem);
+  } catch (error) {
+    res.send(error);
   }
-  catch (error) {
-    res.send(error)
-  }
-})
+});
 
 //delete menus api
 app.delete("/menus/:id", async (req, res) => {
@@ -46,31 +45,26 @@ app.delete("/menus/:id", async (req, res) => {
       return res.status(400).send();
     }
     res.send(deleteMenu);
-  }
-  catch (e) {
+  } catch (e) {
     //server error
     res.status(500).send(e);
   }
-})
+});
 
 //update the student by its id
-app.patch("/menus/:id",async (req,res)=>{
-  try{
-    const _id=req.params.id;
-    const updateitem=await Menus.findByIdAndUpdate(_id,req.body,{
+app.patch("/menus/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const updateitem = await Menus.findByIdAndUpdate(_id, req.body, {
       //show updated data
-      new:true
+      new: true,
     });
     res.send(updateitem);
-    
-  }
-  catch(e){
+  } catch (e) {
     res.status(400).send(e);
-
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`connection is setup at ${port}`);
-})
-
+});
--- Backend/src/db/conn.js
+++ Backend/src/db/conn.js
@@ -1,9 +1,8 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 //connection creation and create database
-mongoose.connect("mongodb://localhost:27017/foodOrdering",{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log("connection successfully"))
-.catch((err)=>console.log(err));
-
-
-
+// mongoose.connect("mongodb://localhost:27017/foodOrdering",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/foodOrdering")
+  .then(() => console.log("connection successfully"))
+  .catch((err) => console.log(err));
